23c8ab22472388af704c7ed85ff86ef3a9ff74f4
Gnzcode <contact@gnzcode.com>
2023-12-26 16:25:00 -0600

    Edit astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 
 export default defineConfig({
+  base: "shise-docs",
   integrations: [tailwind(), mdx()],
   shikiConfig: {
     theme: "light-plus",
